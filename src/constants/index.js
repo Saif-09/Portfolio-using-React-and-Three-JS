3737b45f1ed871973d1ffdb5827350107ba67070
Saif-09 <saifmd238@gmail.com>
2023-08-24 01:00:40 +0530

    added tilt motion card to about

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,7 +42,7 @@ export const navLinks = [
 
 const services = [
     {
-        title: "Web Developer",
+        title: "Fronend Developer",
         icon: web,
     },
     {
@@ -54,7 +54,7 @@ const services = [
         icon: backend,
     },
     {
-        title: "Content Creator",
+        title: "Web Developer",
         icon: creator,
     },
 ];
