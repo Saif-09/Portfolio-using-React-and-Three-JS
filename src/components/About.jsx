38c8525e0eb4a288c7b60dcec22f4d874e954ae0
Saif-09 <saifmd238@gmail.com>
2023-08-24 00:34:25 +0530

    Hero Component Completed also added canvas to the page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import {motion} from 'framer-motion';
+import {styles} from '../style';
+import {services} from '../constants';
+import {fadeIn, textVariant} from '../utils/motion';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview</h2>
+    </motion.div>
+    <motion.p
+    variants={fadeIn("","",0.1,1)}
+    >
+      I'm a skilled software developer with experience in JavaScript and Java, and also proficient in frameworks like React, Node.js and Express.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring ideas to life! 
+
+    </motion.p>
+    </>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-const Computers = () => {
+// Component defining the 3D computer scene
+const Computers = ({ isMobile }) => {
+  // Load the 3D model of the computer
+  const computer = useGLTF('./desktop_pc/scene.gltf');
   return (
-    <div>Computers</div>
-  )
-}
+    <group> {/* A container for grouping scene elements */}
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={Math.PI / 2} 
+        penumbra={1}
+        castShadow
+        intensity={1}
+        shadowMapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 0.45 : 0.75} 
+        position={isMobile ? [0, -1.8, -1.5] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </group>
+  );
+};
 
-export default Computers
+// Main component containing the canvas and scene
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [rotate, setRotate] = useState(false); // Add a state to control rotation
+
+  useEffect(() => {
+    // Listen for changes in screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Initialize `isMobile` state based on screen size
+    setIsMobile(mediaQuery.matches);
+
+    // Handle changes in media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add listener for media query changes
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Clean up when component unmounts
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleCanvasTouch = () => {
+    setRotate(!rotate);
+  };
+
+  return (
+    <Canvas
+    frameloop={rotate ? "always" : "demand"} // Use demand when not rotating
+    onPointerDown={handleCanvasTouch} // Listen for canvas touch
+    shadows
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gl={{ preserveDrawingBuffer: true }}
+  >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ambientLight intensity={2} /> {/* Add soft overall lighting */}
+        <Computers isMobile={isMobile} /> {/* Render the 3D scene */}
+      </Suspense>
+
+      <Preload all /> {/* Preload assets for smoother experience */}
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
